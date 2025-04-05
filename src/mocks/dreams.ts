05ffb3b2bc1445aa50a7af2076d7428937523a11
kevin cuadros <kevindavidcuadros@gmail.com>
2025-04-05 12:09:02 -0500

    create home page

---
+++ src/mocks/dreams.ts
@@ -0,0 +1,80 @@
+
+export const DREAMS = [
+  {
+    id: 1,
+    description: "To travel around the world and experience different cultures",
+    info: "Sarah, 20",
+    position: {
+      desktop: {
+        top: "18%",
+        left: "-10%",
+      },
+    },
+  },
+  {
+    id: 2,
+    description: "To travel around the world and experience different cultures",
+    info: "Sarah, 21",
+    position: {
+      desktop: {
+        top: "15%",
+        left: "75%",
+      },
+    },
+  },
+  {
+    id: 3,
+    description: "To travel around the world and experience different cultures",
+    info: "Sarah, 22",
+    position: {
+      desktop: {
+        top: "65%",
+        left: "75%",
+      },
+    },
+  },
+  {
+    id: 4,
+    description: "To travel around the world and experience different cultures",
+    info: "Sarah, 23",
+    position: {
+      desktop: {
+        top: "52%",
+        left: "-15%",
+      },
+    },
+  },
+  {
+    id: 5,
+    description: "To travel around the world and experience different cultures",
+    info: "Sarah, 24",
+    position: {
+      desktop: {
+        top: "5%",
+        left: "30%",
+      },
+    },
+  },
+   {
+    id: 6,
+    description: "To travel around the world and experience different cultures",
+    info: "Sarah, 25",
+    position: {
+      desktop: {
+        top: "75%",
+        left: "5%",
+      },
+    },
+  },
+  {
+      id: 7,
+      description: "To travel around the world and experience different cultures",
+      info: "Sarah, 26",
+      position: {
+        desktop: {
+          top: "80%",
+          left: "50%",
+        },
+      },
+  }
+];  
